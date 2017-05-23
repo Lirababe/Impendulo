9ef8565009308e7977f6ec1273bd6dc42d5f18ec
Brendan Wood <brendanw@mweb.co.za>
2017-05-23 16:19:43 +0200

    Form Menu For Different Forms
    I added a menu form which has links for the different forms that are part of this project. this will allow any to qickly see what forms and components are part of this project section when they load it .

---
+++ src/Impendulo.Enquiry/frmAllFormInProject.cs
@@ -0,0 +1,48 @@
+﻿using Impendulo.Enquiry.Development.EnquiryV2.Development;
+using Impendulo.Enquiry.Development.InitaialConsultation;
+using Impendulo.Enquiry.Development.InitialDocumentation.ApprenticeshipDocumentation;
+using Impendulo.WizardForm.ClientEnquiry.Development;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Impendulo.Enquiry.Development
+{
+    public partial class frmAllFormInProject : Form
+    {
+        public frmAllFormInProject()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            frmClientEnquiryV2 frm = new frmClientEnquiryV2();
+            frm.ShowDialog();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            frmEnquiryInitialConsultation frm = new frmEnquiryInitialConsultation();
+            frm.ShowDialog();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            frmInitailDocumentation frm = new frmInitailDocumentation();
+            frm.ShowDialog();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            frmNewEnquiry frm = new frmNewEnquiry();
+            frm.ShowDialog();
+        }
+    }
+}
