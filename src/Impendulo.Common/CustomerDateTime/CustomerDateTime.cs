169cdbb67ca1c3a0a85add623bed4db7f3f94677
SbuDeveloper <sbunyambose@gmail.com>
2017-05-24 15:00:42 +0200

    CustomerDateTimeClass
    I added a customer date class in the common project but i am not sure on how to declare a static class into the frmWorkbanchEnquiries.cs, i will at it and try it when i get home.... dont forget to send me the link for working with a chart.. thanx...

---
+++ src/Impendulo.Common/CustomerDateTime/CustomerDateTime.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Impendulo.Common.CustomerDateTime
+{
+    public static class CustomerDateTime
+    {
+        public static DateTime getCustomDateTime(DateTime CurrentDate, int AmountDaysToAdd)
+        {
+            int iCount = 0;
+
+
+            while (!(AmountDaysToAdd == 0))
+            {
+                if ((CurrentDate.DayOfWeek != DayOfWeek.Saturday && CurrentDate.DayOfWeek != DayOfWeek.Sunday))
+                {
+
+                    if (AmountDaysToAdd < 0)
+                    {
+                        CurrentDate = CurrentDate.AddDays(-1);
+                        AmountDaysToAdd++;
+                    }
+                    else
+                    {
+                        CurrentDate = CurrentDate.AddDays(1);
+                        iCount--;
+                    }
+                }
+                else
+                {
+                    if (AmountDaysToAdd < 0)
+                    {
+                        CurrentDate = CurrentDate.AddDays(-1);
+                    }
+                    else
+                    {
+                        CurrentDate = CurrentDate.AddDays(1);
+                    }
+                }
+            }
+            return CurrentDate;
+        }
+    }
+}
