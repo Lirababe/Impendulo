0bd454452ee9619c7712622f60ac49283f1e98c0
Brendan Wood <brendanw@mweb.co.za>
2017-06-06 10:52:05 +0200

    Added Enrollment Selection form
    this form is used to display a list of enrollments associated with the current enquiry.

---
+++ src/Impendulo.Enquiry/EnrollmentSelectionFromEquiry/frmEnrollmentSelectionForEquiry.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Impendulo.Enquiry.Development.EnrollmentSelectionFromEquiry
+{
+    public partial class frmEnrollmentSelectionForEquiry : Form
+    {
+        public frmEnrollmentSelectionForEquiry()
+        {
+            InitializeComponent();
+        }
+    }
+}
